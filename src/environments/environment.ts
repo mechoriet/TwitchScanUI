7a8ff3d142ccdf21f93ebc14cfe75ad80bbcd6ed
mechoriet <kevinworm92@gmail.com>
2025-02-18 15:57:17 +0100

    use clientid from enviorment file instead of hardcoded

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
   version: '0.0.8',
   production: false,
-  backendUrl: 'https://localhost:44309/',
+  backendUrl: 'https://localhost:5001/',
   twitchEmbedUrl: 'localhost',
   frontendUrl: 'http://localhost:4200/',
+  twitchappid: '<FILLIN Client ID>',
 };
