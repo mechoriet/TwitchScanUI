e86b716cacd80cbbec2d1a50698ef4542051bde3
mechoriet <kevinworm92@gmail.com>
2025-02-18 17:21:17 +0100

    change url back

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   version: '0.0.8',
   production: false,
-  backendUrl: 'https://localhost:5001/',
+  backendUrl: 'https://localhost:44309/',
   twitchEmbedUrl: 'localhost',
   frontendUrl: 'http://localhost:4200/',
   twitchappid: '<FILLIN Client ID>',
